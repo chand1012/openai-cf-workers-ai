d179a09edf6061ae8259531b398422a904d5d390
devcxl <64475363+devcxl@users.noreply.github.com>
2024-04-10 18:44:50 +0800

    add: BearerAuthentication middleware

--- index.js
+++ index.js
@@ -10,31 +10,41 @@ import { getImageHandler, imageGenerationHandler } from './routes/image';
 const { preflight, corsify } = createCors();
 
 // Create a new router
-const router = Router();
+const router = Router({ base: '/v1' });
+
+function extractToken(authorizationHeader) {
+	if (authorizationHeader) {
+		const parts = authorizationHeader.split(' ');
+		if (parts.length === 2 && parts[0] === 'Bearer') {
+			return parts[1];
+		}
+	}
+	return null;
+}
+// MIDDLEWARE: withAuthenticatedUser - embeds user in Request or returns a 401
+const bearerAuthentication = (request, env) => {
+	const authorizationHeader = request.headers.get('Authorization');
+	if (!authorizationHeader) {
+		return error(401, 'Unauthorized');
+	}
+	const access_token = extractToken(authorizationHeader)
+	if (env.ACCESS_TOKEN !== access_token) {
+		return error(403, 'Forbidden');
+	}
+};
 
 // CORS, see https://itty.dev/itty-router/cors
+router.all('*', preflight);
+
 router
-	// embed preflight upstream to handle all OPTIONS requests
-	.all('*', preflight);
-
-// chat completion
-router.post('/chat/completions', chatHandler);
-
-// legacy completions
-router.post('/completions', completionHandler);
-
-// embeddings
-router.post('/embeddings', embeddingsHandler);
-
-// audio transcriptions
-router.post('/audio/transcriptions', transcriptionHandler);
-
-// audio translations
-router.post('/audio/translations', translationHandler);
-
-router.post('/images/generations', imageGenerationHandler);
-
-router.get('/images/get/:name', getImageHandler);
+	.all('*', bearerAuthentication)
+	.post('/chat/completions', chatHandler)
+	.post('/completions', completionHandler)
+	.post('/embeddings', embeddingsHandler)
+	.post('/audio/transcriptions', transcriptionHandler)
+	.post('/audio/translations', translationHandler)
+	.post('/images/generations', imageGenerationHandler)
+	.get('/images/get/:name', getImageHandler);
 
 // 404 for everything else
 router.all('*', () => new Response('404, not found!', { status: 404 }));
